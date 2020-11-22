3c1fc713d05d9ee68c69ad42b90e5565966d81bf
shyheng <shy2210278285@aliyun.com>
2020-11-22 16:34:59 +0800

    2020.11.22 extemC

---
+++ extemC.cpp
@@ -0,0 +1,9 @@
+﻿#include<iostream>
+#include "myadd.h"
+using namespace std;
+
+int main()
+{
+	cout << myadd(2, 3) << endl;
+	return 0;
+}
---
+++ myadd.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#if __cplusplus
+extern "C"{
+#endif
+
+	int myadd(int a, int b);
+
+#if __cplusplus
+}
+#endif
