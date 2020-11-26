290f81f451ebcef710e1294030991b6f0b6c41f9
shyheng <shy2210278285@aliyun.com>
2020-11-26 20:14:02 +0800

    2020.11.26体积面积  构造函数

---
+++ Constructor.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+#include<string.h>
+#include<string>
+#define _CRT_SECURE_NO_WARNINGS
+//构造函数
+using namespace std;
+
+class Preson
+{
+public:
+	//构造函数 1 函数名和类名一致 没有返回值 不能写void 可以返回值 可以发生函数重载
+	Preson(int age, string name)
+	{
+		m_age = age;
+		m_name = name;
+	}
+	//析构函数 函数名前面加上~ 没有返回值 不可以有参数 不能发生函数重载
+	~Preson()
+	{
+		cout << "析构函数" << endl;
+	}
+	int m_age;
+	string m_name;
+};
+
+void text1()
+{
+	Preson p1(10, "shy");//构造函数是在实例化对象时创建，就是在内存开辟是就会被调用 
+//销毁之前 自动调用析构函数 
+}
+
+int main()
+{
+	text1();
+	return 0;
+}
---
+++ Cube.cpp
@@ -0,0 +1,101 @@
+﻿#include<iostream>
+#include<string.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+using namespace std;
+
+class Cube
+{
+public:
+	void set_L(int l)
+	{
+		L = l;
+	}
+	void set_W(int w)
+	{
+		W = w;
+	}
+	void set_H(int h)
+	{
+		H = h;
+	}
+
+	int get_L()
+	{
+		return L;
+	}
+	int get_W()
+	{
+		return W;
+	}
+	int get_H()
+	{
+		return H;
+	}
+
+	//求立方体的体积
+	int get_cube_V()
+	{
+		return L * W * H;
+	}
+	//求立方体面积
+	int get_cube_S()
+	{
+		return 2 * W * L + 2 * W * H + 2 * L * H;
+	}
+	//两个立方体是否相等
+	bool compare_cube(Cube &c1)
+	{
+		return c1.get_L() == L && c1.get_W() == W && c1.get_H() == H;
+	}
+private: 
+	int L;
+	int W;
+	int H;
+};
+
+
+bool comapre_cube(Cube& c1, Cube& c2)
+{
+	return c1.get_L() == c2.get_L() && c1.get_W() == c2.get_W() && c1.get_H() == c2.get_H();
+}
+
+
+void text1()
+{
+	Cube c1;
+	c1.set_L(100);
+	c1.set_W(200);
+	c1.set_W(300);
+	cout << c1.get_cube_S() << endl;
+	cout << c1.get_cube_V() << endl;
+
+	Cube c2;
+	c2.set_L(100);
+	c2.set_W(200);
+	c2.set_W(300);
+
+	if (c1.compare_cube(c2))
+	{
+		cout << "立方体相等" << endl;
+	}
+	else
+	{
+		cout << "立方体不相等" << endl;
+	}
+
+
+	if (comapre_cube(c1, c2))
+	{
+		cout << "立方体相等" << endl;
+	}
+	else
+	{
+		cout << "立方体不相等" << endl;
+	}
+}
+int main()
+{
+	text1();
+	return 0;
+}
