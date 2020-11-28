0188a689d71459ec1e36a2f0671c2c9afc529997
shyheng <shy2210278285@aliyun.com>
2020-11-28 20:09:46 +0800

    类成员

---
+++ Class.cpp
@@ -0,0 +1,76 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<string.h>
+
+using namespace std;
+
+class Phone
+{
+public:
+	Phone(string name)
+	{
+		pho_name = name;
+		cout << " phone构造函数" << endl;
+	}
+	~Phone()
+	{
+		cout << "析构" << endl;
+	}
+	string pho_name;
+};
+
+class Game
+{
+public:
+	Game(string name)
+	{
+		game_name = name;
+		cout << "game构造函数" << endl;
+	}
+	~Game()
+	{
+		cout << "析构" << endl;
+	}
+	string game_name;
+
+};
+
+class Person
+{
+public:
+	//Person(string per_name1,string pho_name,string g_name)
+	//{
+	//	per_name = per_name1;
+	//	phone.pho_name = pho_name;
+	//	game.game_name = g_name;
+	//}
+	Person(string per_name1, string pho_name, string g_name) :per_name(per_name1), phone(pho_name), game(g_name) {
+		cout << " person构造函数" << endl;
+	}
+	void show()
+	{
+		cout << "人" << phone.pho_name << "玩着" << game.game_name << endl;
+	}
+	~Person()
+	{
+		cout << "析构" << endl;
+	}
+	string per_name;
+	Game game;
+	Phone phone;
+	
+	
+};
+
+void text1()
+{
+	Person p1("shy","zph","ll");
+	p1.show();
+}
+
+int main()
+{
+	text1();
+	return 0;
+}
