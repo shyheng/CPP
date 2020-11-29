a4f5333f1d412f59d46d485a9453fc54c7ec6e5b
shyheng <shy2210278285@aliyun.com>
2020-11-29 15:53:16 +0800

    重载<<

---
+++ Heavy_load-《.cpp
@@ -0,0 +1,42 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<string.h>
+
+using namespace std;
+
+class person
+{
+	friend ostream& operator<<(ostream& cout, person& p);
+public:
+
+	person(int age)
+	{
+		this->age = age;
+	}
+	person operator+(person& p2)
+	{
+		person p(this->age + p2.age);
+		return p;
+	}
+private:
+	int age;
+};
+
+ostream& operator<<(ostream& cout, person& p)
+{
+	cout << p.age;
+	return cout;
+}
+
+void text1()
+{
+	person p1(10);
+	cout << p1<<endl;
+	// operator<<(cout,p1)  cout .operator<<(p1)
+}
+int main()
+{
+	text1();
+	return 0;
+}
