be0c2c429aa15edd9f6d17c51127b26a8f26430f
shyheng <shy2210278285@aliyun.com>
2020-11-29 15:49:39 +0800

    友元关键字

---
+++ Friend.cpp
@@ -0,0 +1,37 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string.h>
+#include<string>
+#include<stdlib.h>
+
+using namespace std;
+
+class Building
+{
+	friend void print_Building(Building& b);
+public:
+	Building(string hall, string bedroom)
+	{
+		this->bedroom = bedroom;
+		this->hall = hall;
+	}
+	string hall;
+private:
+	string bedroom;
+};
+
+void print_Building(Building &b)
+{
+	cout << b.hall << " " << b.bedroom << endl;
+}
+
+void text1()
+{
+	Building b1("家","房间");
+	print_Building(b1);
+}
+int main()
+{
+
+	return 0;
+}
