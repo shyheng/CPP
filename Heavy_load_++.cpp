be0df7be42f939d6e28ce12a80ebd3ef1d876b48
shyheng <shy2210278285@aliyun.com>
2020-11-30 20:28:14 +0800

    重载自加

---
+++ Heavy_load_++.cpp
@@ -0,0 +1,48 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<string.h>
+#include<stdlib.h>
+
+using namespace std;
+
+class Myint
+{
+public:
+	Myint(int num)
+	{
+		this->num = num;
+	}
+	Myint& operator++()
+	{
+		this->num = this->num + 1;
+		return *this;
+	}
+	Myint operator++(int)
+	{
+		Myint tmp = *this;
+		//加加
+		this->num = this->num + 1;
+		return tmp;
+	}
+	int num;
+};
+
+ostream& operator<<(ostream& cuot, Myint& p)
+{
+	cout << p.num;
+	return cout;
+}
+void text1()
+{
+	Myint p1(10);
+	cout << p1 << endl;
+	++p1;//operator++(p1) p1.operator++()
+	cout << ++p1 << endl;
+	cout << p1++ << endl; //p1.operator++(int)
+}
+int main()
+{
+	text1();
+	return 0;
+}
