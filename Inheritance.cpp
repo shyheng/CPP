7b90ff213623878da7a0198ceb393e210e9be4c7
shyheng <shy2210278285@aliyun.com>
2020-12-01 13:15:35 +0800

    继承text

---
+++ Inheritance.cpp
@@ -0,0 +1,42 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<string.h>
+#include<stdlib.h>
+
+using namespace std;
+
+//基类
+class Animal
+{
+public:
+	int age;
+	void print()
+	{
+		cout << age << endl;
+	}
+};
+
+class Dog :public Animal
+{
+public:
+	int tail_len;
+	//int age;
+	//void print()
+	//{
+	//	cout << age << endl;
+	//}
+
+};
+
+void text1()
+{
+	Dog d;
+	d.print();
+}
+
+int mian()
+{
+	text1();
+	return 0;
+}
