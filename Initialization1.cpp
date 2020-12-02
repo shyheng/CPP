963da46bf103b7d34f209eccd9b11b5c6d3023a0
shyheng <shy2210278285@aliyun.com>
2020-12-02 18:48:36 +0800

    继承控制权

---
+++ Initialization1.cpp
@@ -0,0 +1,54 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<string.h>
+//继承方式的控制权限
+using namespace std;
+
+class Base
+{
+public:
+	int a;
+protected:
+	int b;
+private:
+	int c;
+};
+
+//公有的继承方式 基类
+class A :public Base
+{
+//public:
+//	int a;
+//protected:
+//	int b;
+//private:
+//	int c;
+public:
+	int d;
+	void show()
+	{
+		//子类的成员函数去访问父类成员 子类不可以访问父类的私有成员
+		cout << a << b << endl;
+	}
+};
+
+class B :protected Base
+{
+public:
+	int d;
+};
+
+void text1()
+{
+	A p;
+	//p通过类外可以访问公有权
+	p.a = 10;
+	p.d = 20;
+}
+
+int main()
+{
+
+	return 0;
+}
