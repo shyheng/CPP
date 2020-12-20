700e5fd640f1b89dfe83ac73cdbcf2538379e311
shyheng <shy2210278285@aliyun.com>
2020-12-20 19:39:39 +0800

    内模板实现

---
+++ Template_inside_outside.cpp
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <stdlib.h>
+//内模板实现
+using namespace std;
+
+template <class T1,class T2>
+class Person
+{
+public:
+	Person(T1 a, T2 b)
+	{
+		this->a = a;
+		this->b = b;
+	}
+	void show()
+	{
+		cout << a << " " << b << endl;
+	}
+	T1 a;
+	T2 b;
+};
+
+void test1()
+{
+	Person<int,string> p(10, "shy");
+	p.show();
+}
+
+int main()
+{
+	return 0;
+}
