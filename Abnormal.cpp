c26a0ccae9da633b716516fa17f178a215f4d368
shyheng <shy2210278285@aliyun.com>
2020-12-21 20:01:11 +0800

    异常基本概念

---
+++ Abnormal.cpp
@@ -0,0 +1,30 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <stdlib.h>
+//异常基本概念
+using namespace std;
+
+int mydive(int a, int b)
+{
+	if (b == 0)
+		return -1;//errno = 2
+
+	return a / b;
+}
+
+void test1()
+{
+	int ret = mydive(1, -1);
+	if (ret == -1)
+	{
+		cout << "除数为0" << endl;//perror("");
+	}
+}
+
+int main()
+{
+	test1();
+	return 0;
+}
