3def677ca1220e36242a9f8100d3581d98f44f2b
shyheng <shy2210278285@aliyun.com>
2020-12-21 16:24:36 +0800

    常量转换

---
+++ Const_cast.cpp
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <stdlib.h>
+//常量转换
+using namespace std;
+//一般用来加const或去const
+void test1()
+{
+	int* p1 = NULL;
+	const int* p2 = NULL;
+	//int* p1 = static_cast<int*>(p2);
+	p1 = const_cast<int*>(p2);
+	p2 = const_cast<const int*>(p1);
+}
+
+int main()
+{
+	return 0;
+}
