7c631f987303feb17358bc4b0f0e8b24547f77b2
shyheng <shy2210278285@aliyun.com>
2020-12-21 20:50:08 +0800

    栈解旋

---
+++ Unwinding.cpp
@@ -0,0 +1,51 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <stdlib.h>
+//栈解旋
+using namespace std;
+
+class Person
+{
+public:
+	Person(string name)
+	{
+		cout << "构造" << endl;
+		this->name = name;
+	}
+	~Person()
+	{
+		cout << "析构" << endl;
+	}
+	string name;
+};
+
+void fun()
+{
+	Person p2("shy");
+	Person p3("shy");
+	cout << "001" << endl;
+	throw 1;
+	
+}
+
+void test1()
+{
+	try
+	{
+		Person p1("shy");
+		fun();
+	}
+	catch (int)
+	{
+		cout << "002" << endl;
+		cout << "捕获到异常" << endl;
+	}
+}
+
+int main()
+{
+	test1();
+	return 0;
+}
