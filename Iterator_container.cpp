2731bf54ad8ee8310c2ee6b89ce0166b02ced6a1
shyheng <shy2210278285@aliyun.com>
2020-12-23 16:22:38 +0800

    迭代器指向的是一个容器

---
+++ Iterator_container.cpp
@@ -0,0 +1,43 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <string.h>
+#include <stdexcept>
+#include <vector>
+#include <algorithm>
+//迭代器指向的是一个容器
+using namespace std;
+
+void test1()
+{
+	vector<vector<int>> v;
+	vector<int> v1;
+	vector<int> v2;
+	vector<int> v3;
+	for (int i = 0; i < 3; i++)
+	{
+		v1.push_back(i);
+		v2.push_back(i+10);
+		v3.push_back(i + 100);
+	}
+	v.push_back(v1);
+	v.push_back(v2);
+	v.push_back(v3);
+	vector<vector<int>>::iterator it = v.begin();
+	for (; it != v.end(); it++)
+	{
+		//*it 得到的是vector<int>
+		vector<int>::iterator it1 = (*it).begin();
+		for (; it1 != (*it).end(); it1++)
+		{
+			cout << *it1 << endl;
+		}
+	}
+}
+
+int main()
+{
+	test1();
+	return 0;
+}
