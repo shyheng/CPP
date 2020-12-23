d310c198700cacefa443b958659fb63ca4f4586e
shyheng <shy2210278285@aliyun.com>
2020-12-23 15:41:48 +0800

    迭代器通过for_each遍历

--- Iterator.cpp
+++ Iterator.cpp
@@ -6,8 +6,13 @@
 #include <stdlib.h>
 #include <algorithm>
 #include <iostream>
-
+//迭代器
 using namespace std;
+
+void print(int a)
+{
+	cout << a << endl;
+}
 
 void test1()
 {
@@ -19,11 +24,12 @@ void test1()
 	//如果需要访问容器的元素 需要拿到容器首元素的迭代器（指针）
 	vector<int>::iterator it_start = v.begin();
 	vector<int>::iterator it_end = v.end();
-	for (it_start; it_start != it_end; it_start++)
-	{
-		cout << *it_start << " ";
-	}
-	cout << endl;
+	//for (; it_start != it_end; it_start++)
+	//{
+	//	cout << *it_start << " ";
+	//}
+	//cout << endl;
+	for_each(it_start, it_end,print);
 }
 
 int main()
