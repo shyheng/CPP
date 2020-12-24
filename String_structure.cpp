65cc5e5139385d73dc6964698aebcf7c6a011507
shyheng <shy2210278285@aliyun.com>
2020-12-24 19:51:24 +0800

    string容器的构造函数

---
+++ String_structure.cpp
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <stdlib.h>
+#include <stdexcept>
+#include <vector>
+#include <algorithm>
+//string容器的构造函数
+using namespace std;
+
+void test1()
+{
+	string str;
+	string str1("hello");
+	string str2(str1);
+	string str3(5, 'k');
+	cout << str1 << endl;
+	cout << str2 << endl;
+	cout << str3 << endl;
+}
+
+int main()
+{
+	test1();
+	return 0;
+}
