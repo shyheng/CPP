3d55fca8509b2dfc63e037507a101a7b77ffc8c0
shyheng <shy2210278285@aliyun.com>
2020-12-26 16:53:17 +0800

    内建函数对象

---
+++ Built-in_function_object.cpp
@@ -0,0 +1,53 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <stdlib.h>
+#include <stdexcept>
+#include <vector>
+#include <deque>
+#include <algorithm>
+#include <functional>
+
+//内建函数对象
+using namespace std;
+
+void test1()
+{
+	negate<int> p;//取反数
+	cout << p(5) << endl;//-5
+	cout << negate<int>()(2) << endl;
+}
+
+void test2()
+{
+	cout << plus<int>()(2, 3) << endl;
+}
+
+class Print
+{
+public:
+	void operator()(int a)
+	{
+		cout << a << endl;
+	}
+};
+
+
+void test3()
+{
+	vector<int> v;
+	v.push_back(1);
+	v.push_back(3);
+	v.push_back(5);
+	v.push_back(2);
+	//sort(v.begin(), v.end(),greater<int>());
+	sort(v.begin(), v.end(),less<int>());
+	for_each(v.begin(), v.end(),Print() );
+}
+
+int main()
+{
+	test1();
+	return 0;
+}
